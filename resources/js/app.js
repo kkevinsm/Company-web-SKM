e1a4276eb777fabb74d1516f2dab84dc7e70b9ed
Kev <mac@Kevs-MacBook-Pro.local>
2025-07-30 21:56:50 +0700

    feat: add section 5 done

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,14 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
+import Splide from '@splidejs/splide';
+import '@splidejs/splide/css';
+window.Splide = Splide;
+
+import { tns } from 'tiny-slider/src/tiny-slider';
+import 'tiny-slider/dist/tiny-slider.css';
+window.tns = tns;
+
 window.Alpine = Alpine;
 
 Alpine.start();
